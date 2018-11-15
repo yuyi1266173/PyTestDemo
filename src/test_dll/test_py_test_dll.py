c295b78e3e10163751c1bf8cba3a5b0356225b47
wuyu <wuyu@xinktech.com>
2018-11-15 09:58:30 +0800

    添加注释

--- src/test_dll/test_py_test_dll.py
+++ src/test_dll/test_py_test_dll.py
@@ -1,4 +1,10 @@
 
+"""
+C扩展demo程序
+py_test_dll模块引用于py_test_dll.pyd文件，放在src目录下
+helloext.pyd用VS2017 python扩展模块项目生成，项目路径：D:\project\vs_prj\py_test_dll
+"""
+
 import py_test_dll
 
 print(py_test_dll.Add(3, 5))
--- src/test_hello_ext.py
+++ src/test_hello_ext.py
@@ -1,4 +1,9 @@
 
+"""
+C扩展demo程序
+py_test_dll模块引用于 helloext.pyd文件，放在src目录下
+helloext.pyd用VS2017 python扩展模块项目生成，项目路径：D:\project\vs_prj\helloext
+"""
 
 import helloext
 
